aa5a7baa21b49153336185c69344e6bd84b6b9aa
blacnova <147455788+smokeloud1@users.noreply.github.com>
2023-10-22 03:36:21 -0600

    Add files via upload

--- script.js
+++ script.js
@@ -9,11 +9,9 @@ document.addEventListener("DOMContentLoaded", function () {
     webpToPngText.classList.add("disabledText");
 
     fileInput.addEventListener("change", handleFileSelection);
-
     downloadButton.addEventListener("click", downloadConvertedImage);
 
     let convertedBlob;
-
     const supportedFormats = ['AVIF', 'SVG', 'PDF'];
     let currentFormatIndex = 0;
 
@@ -96,82 +94,181 @@ document.addEventListener("DOMContentLoaded", function () {
     lightIcon.addEventListener('click', applyLightMode);
 
     function applyDarkMode() {
-        darkIcon.style.display = 'none';
-        lightIcon.style.display = 'inline-block';
         document.body.classList.add('dark-mode');
         document.body.classList.remove('light-mode');
         whitelogoContainer.style.display = 'block';
         logoContainer.style.display = 'none';
-        darkIcon.style.display = 'none';
-        lightIcon.style.display = 'inline-block';
         webpToPngText.classList.add("dark-mode-text");
         localStorage.setItem('darkMode', 'dark');
+        toggleIconVisibility();
     }
 
     function applyLightMode() {
-        lightIcon.style.display = 'none';
-        darkIcon.style.display = 'inline-block';
         document.body.classList.add('light-mode');
         document.body.classList.remove('dark-mode');
         whitelogoContainer.style.display = 'none';
         logoContainer.style.display = 'block';
-        lightIcon.style.display = 'none';
-        darkIcon.style.display = 'inline-block';
         webpToPngText.classList.remove("dark-mode-text");
         localStorage.setItem('darkMode', 'light');
+        toggleIconVisibility();
+    }
+
+    function toggleIconVisibility() {
+        darkIcon.style.display = (document.body.classList.contains('dark-mode')) ? 'none' : 'inline-block';
+        lightIcon.style.display = (document.body.classList.contains('dark-mode')) ? 'inline-block' : 'none';
     }
 
     let conversionStartTime;
 
     async function handleFileSelection() {
-    downloadButton.disabled = true;
-    webpToPngText.classList.add("disabledText");
-    webpToPngText.style.display = "none";
+        downloadButton.disabled = true;
+        webpToPngText.classList.add("disabledText");
+        webpToPngText.style.display = "none";
 
-    const file = fileInput.files[0];
+        const file = fileInput.files[0];
 
-    if (file && (file.type === "image/webp" || file.type === "image/avif" || file.type === "image/svg+xml")) {
-        try {
-            console.log("Starting conversion...");
-            conversionStartTime = performance.now();
+        if (file && (file.type === "image/webp" || file.type === "image/avif" || file.type === "image/svg+xml")) {
+            try {
+                console.log("Starting conversion...");
+                conversionStartTime = performance.now();
 
-            let blob;
+                let blob;
 
-            if (file.type === "image/svg+xml") {
-                blob = new Blob([file], { type: "image/svg+xml" });
-            } else {
-                const dataURL = await readFileAsDataURL(file);
-                const convertedData = await convertToPNG(dataURL);
-                blob = new Blob([convertedData], { type: "image/png" });
+                 if (file.type === "image/svg+xml") {
+                const svgData = await readFileAsDataURL(file);
+                blob = await convertToPNGFromSVG(svgData);
+                } else {
+                    const dataURL = await readFileAsDataURL(file);
+                    const convertedData = await convertToPNG(dataURL);
+                    blob = new Blob([convertedData], { type: "image/png" });
+                }
+
+                convertedBlob = blob;
+                displayConvertedImage(convertedBlob);
+
+                downloadButton.disabled = false;
+                webpToPngText.style.display = "block";
+                const conversionEndTime = performance.now();
+                const conversionTime = conversionEndTime - conversionStartTime;
+                console.log("Conversion took " + conversionTime.toFixed(2) + " milliseconds");
+                webpToPngText.classList.add("display-text");
+                webpToPngText.innerHTML = `Conversion took ${conversionTime.toFixed(2)} milliseconds`;
+            } catch (error) {
+                console.error("Conversion failed:", error);
+                alert("Conversion failed. Please try again.");
+                resetFileInputAndOutput();
             }
+        } else if (file && file.type === "application/pdf") {
+            try {
+                console.log("Rendering PDF...");
+                conversionStartTime = performance.now();
 
-            convertedBlob = blob;
-            displayConvertedImage(convertedBlob);
+                // Make sure to include pdfjsLib
+                const pdfjsLib = window['pdfjs-dist/build/pdf'];
+                const workerSrc = 'pdf.worker.js'; // Adjust the path to your pdf.worker.js file
+                pdfjsLib.GlobalWorkerOptions.workerSrc = workerSrc;
 
-            downloadButton.disabled = false;
-            webpToPngText.style.display = "block";
-            const conversionEndTime = performance.now();
-            const conversionTime = conversionEndTime - conversionStartTime;
-            console.log("Conversion took " + conversionTime.toFixed(2) + " milliseconds");
-            webpToPngText.classList.add("display-text");
-            webpToPngText.innerHTML = `Conversion took ${conversionTime.toFixed(2)} milliseconds`;
-        } catch (error) {
-            console.error("Conversion failed:", error);
-            alert("Conversion failed. Please try again.");
-            fileInput.value = "";
-            outputImage.style.display = "none";
+                const pdfData = await readFileAsArrayBuffer(file);
+                const pdfDocument = await pdfjsLib.getDocument({ data: pdfData }).promise;
+                const pdfBlob = await renderPDF(pdfDocument);
+
+                convertedBlob = pdfBlob;
+                displayConvertedImage(convertedBlob);
+
+                downloadButton.disabled = false;
+                webpToPngText.style.display = "block";
+                const conversionEndTime = performance.now();
+                const conversionTime = conversionEndTime - conversionStartTime;
+                console.log("Rendering took " + conversionTime.toFixed(2) + " milliseconds");
+                webpToPngText.classList.add("display-text");
+                webpToPngText.innerHTML = `Rendering took ${conversionTime.toFixed(2)} milliseconds`;
+            } catch (error) {
+                console.error("PDF rendering failed:", error);
+                alert("PDF rendering failed. Please try again.");
+                resetFileInputAndOutput();
+            }
+        } else {
+            alert("Please select a valid file (WebP, AVIF, SVG, or PDF).");
+            resetFileInputAndOutput();
         }
-    } else {
-        alert("Please select a valid WebP, AVIF, or SVG file.");
-        fileInput.value = "";
-        outputImage.style.display = "none";
+    }
+
+    async function renderPDF(pdfDocument) {
+    try {
+        const firstPage = await pdfDocument.getPage(1);
+        const viewport = firstPage.getViewport({ scale: 1.5 });
+
+        // Create a canvas to render the PDF page
+        const canvas = document.createElement('canvas');
+        const context = canvas.getContext('2d');
+        canvas.width = viewport.width;
+        canvas.height = viewport.height;
+
+        // Render the PDF page onto the canvas
+        await firstPage.render({
+            canvasContext: context,
+            viewport: viewport
+        }).promise;
+
+        // Convert the canvas to a Blob representing the image
+        return new Promise(resolve => {
+            canvas.toBlob(blob => {
+                resolve(blob);
+            }, 'image/png');
+        });
+    } catch (error) {
+        console.error('PDF rendering failed:', error);
+        throw error; // Rethrow the error to handle it in the calling function
     }
 }
 
-async function convertToPNG(dataURL) {
-    const response = await fetch(dataURL);
-    const blob = await response.blob();
+    // Function to read a file as an ArrayBuffer
+    function readFileAsArrayBuffer(file) {
+        return new Promise((resolve, reject) => {
+            const reader = new FileReader();
 
+            reader.onload = () => {
+                resolve(reader.result);
+            };
+
+            reader.onerror = reject;
+
+            reader.readAsArrayBuffer(file);
+        });
+    }
+
+    async function readPDFAsDataURL(file) {
+        return new Promise((resolve, reject) => {
+            const reader = new FileReader();
+            reader.onload = () => resolve(reader.result);
+            reader.onerror = reject;
+            reader.readAsDataURL(file);
+        });
+    }
+
+    function displayPDF(pdfBlob) {
+        console.log("Displaying PDF...");
+        const pdfViewer = document.createElement('iframe');
+        pdfViewer.src = URL.createObjectURL(pdfBlob);
+        pdfViewer.style.width = '100%';
+        pdfViewer.style.height = '500px';
+        document.body.appendChild(pdfViewer);
+    }
+
+    function resetFileInputAndOutput() {
+        fileInput.value = "";
+        outputImage.style.display = "none";
+    }
+
+    function downloadPDF() {
+        console.log("Downloading PDF...");
+        const downloadLink = document.createElement("a");
+        downloadLink.href = URL.createObjectURL(pdfBlob);
+        downloadLink.download = "document.pdf";
+        downloadLink.click();
+        URL.revokeObjectURL(downloadLink.href);
+    }
+	async function convertToPNGFromSVG(svgData) {
     return new Promise((resolve, reject) => {
         const img = new Image();
 
@@ -179,20 +276,17 @@ async function convertToPNG(dataURL) {
             const canvas = document.createElement('canvas');
             const context = canvas.getContext('2d');
 
-            // Set canvas dimensions to image dimensions
             canvas.width = img.width;
             canvas.height = img.height;
 
-            // Draw the image on the canvas
             context.drawImage(img, 0, 0);
 
-            // Convert the canvas content to a PNG blob
             canvas.toBlob(
                 (blob) => {
                     resolve(blob);
                 },
                 'image/png',
-                1 // Quality (from 0 to 1)
+                1
             );
         };
 
@@ -200,9 +294,42 @@ async function convertToPNG(dataURL) {
             reject(error);
         };
 
-        img.src = URL.createObjectURL(blob);
+        img.src = `data:image/svg+xml,${encodeURIComponent(svgData)}`;
     });
 }
+
+    async function convertToPNG(dataURL) {
+        const response = await fetch(dataURL);
+        const blob = await response.blob();
+
+        return new Promise((resolve, reject) => {
+            const img = new Image();
+
+            img.onload = function () {
+                const canvas = document.createElement('canvas');
+                const context = canvas.getContext('2d');
+
+                canvas.width = img.width;
+                canvas.height = img.height;
+
+                context.drawImage(img, 0, 0);
+
+                canvas.toBlob(
+                    (blob) => {
+                        resolve(blob);
+                    },
+                    'image/png',
+                    1
+                );
+            };
+
+            img.onerror = function (error) {
+                reject(error);
+            };
+
+            img.src = URL.createObjectURL(blob);
+        });
+    }
 
     function displayConvertedImage(imageBlob) {
         console.log("Displaying converted image...");
@@ -224,32 +351,44 @@ async function convertToPNG(dataURL) {
     }
 
     async function readFileAsDataURL(file) {
-        return new Promise((resolve, reject) => {
-            const reader = new FileReader();
-            reader.onload = () => resolve(reader.result);
-            reader.onerror = reject;
-            reader.readAsDataURL(file);
-        });
-    }
+    return new Promise((resolve, reject) => {
+        const reader = new FileReader();
 
-     function downloadConvertedImage() {
+        reader.onload = () => {
+            if (file.type === "image/svg+xml") {
+                // For SVG files, resolve with the raw text content
+                resolve(reader.result);
+            } else {
+                // For other file types, resolve with the data URL
+                resolve(reader.result);
+            }
+        };
+
+        reader.onerror = reject;
+
+        if (file.type === "image/svg+xml") {
+            // For SVG files, read as text
+            reader.readAsText(file);
+        } else {
+            // For other file types, read as data URL
+            reader.readAsDataURL(file);
+        }
+    });
+}
+
+    function downloadConvertedImage() {
         console.log("Downloading converted image...");
         if (convertedBlob) {
             try {
                 const downloadLink = document.createElement("a");
                 downloadLink.href = URL.createObjectURL(convertedBlob);
 
-                // Set the appropriate file extension based on the selected format
                 const fileExtension = formatSelect.value.toLowerCase();
                 downloadLink.download = `converted_image.${fileExtension}`;
 
-                // Trigger the download
                 downloadLink.click();
-
-                // Clean up
                 URL.revokeObjectURL(downloadLink.href);
 
-                // Update the display text
                 webpToPngText.classList.add("display-text");
                 webpToPngText.innerHTML = `File Saved`;
             } catch (error) {
@@ -258,7 +397,4 @@ async function convertToPNG(dataURL) {
             }
         }
     }
-
-    // ... (unchanged part)
-
 });
